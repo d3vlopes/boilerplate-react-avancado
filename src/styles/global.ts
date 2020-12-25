e0431ab24bd78960b2cc1cf9e421eb88cb858be2
Leandro Lopes <leandro-lopes46@hotmail.com>
2020-12-25 17:10:47 -0300

    Add font on global.ts

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,10 +1,23 @@
 import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyles = createGlobalStyle`
+ /* Exemplo de carregamento da fonte
+  @font-face {
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 300;
+    src: local(''),
+        url('/fonts/poppins-v15-latin-300.woff2') format('woff2')
+    }
+  */
+ 
+
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
   }
 
   html {
@@ -16,7 +29,7 @@ const GlobalStyles = createGlobalStyle`
   }
 
   body {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+    font-family: 'Poppins', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
   }
 `
 

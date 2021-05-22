a55a3ff134326be15e174fa6d62ed8c6a29e7176
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-05-22 17:25:58 -0300

    Add apollo and config codegen for generate types of queries

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,6 +18,7 @@ export async function getServerSideProps() {
   return {
     props: {
       // data: data,
+      // Se existir algo no cache, ele pega
       // initialApolloState: apolloClient.cache.extract(),
     },
   }

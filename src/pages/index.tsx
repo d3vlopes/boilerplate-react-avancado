80b8a29dd66d3ce1f131ff1f967d50fa53d6ee33
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-05-22 16:50:33 -0300

    Creates the graphql folder structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,24 @@
+// import { initializeApollo } from 'utils/apollo'
+// import { SAMPLE_QUERY } from 'graphql/queries/sample'
+
 import Main from 'components/Main'
 
 export default function Home() {
+  // if (props.data) return <p>{JSON.stringify(props.data, null, 2)}</p>
+
   return <Main />
+}
+
+export async function getServerSideProps() {
+  // const apolloClient = initializeApollo()
+
+  // const { data } = await apolloClient.query({ query: SAMPLE_QUERY })
+
+  // retorno dos dados
+  return {
+    props: {
+      // data: data,
+      // initialApolloState: apolloClient.cache.extract(),
+    },
+  }
 }
